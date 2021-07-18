8c29ac660787844cce1178839161b852b85f37f0
Emily Foley <efoley326@gmail.com>
2021-07-18 14:58:54 -0400

    admin tweak?

--- choose_adventure/settings.py
+++ choose_adventure/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-default_auto_field = 'django.db.models.AutoField'
 
 
 STATIC_URL = '/static/'
