6df9e6ed3ff323049f2778f80bc8fa14869dc615
Emily Foley <efoley326@gmail.com>
2021-07-18 14:25:13 -0400

    settings add

--- choose_adventure/settings.py
+++ choose_adventure/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'registration',
     'library',
 ]
@@ -105,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# AUTH_USER_MODEL = 'library.UserProfile'
 
 
 # Internationalization
--- library/models.py
+++ library/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import constraints, ManyToOneRel
 from django.contrib.auth.models import AbstractUser
+# from choose_adventure.settings import AUTH_USER_MODEL
 
 class User(AbstractUser):
     def __str__(self):
